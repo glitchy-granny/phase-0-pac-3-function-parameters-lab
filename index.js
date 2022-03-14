bff9fbb15bf4324ef04c49c8e0e8237ed68675dd
Pamela <pamela.d.daniels@gmail.com>
2022-03-13 23:04:49 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name = 'Aki') {
+    return 'Hi, my name is '+ name + '.';
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguage(name = `Gracie`, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional (name = `Gracie`, language = `JavaScript`) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
